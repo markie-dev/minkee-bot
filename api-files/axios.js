c60f98ae424ab6c28f198a4d57b1bacb69130b8c
marcus <marcus.d.ellison@gmail.com>
2020-10-28 22:41:22 -0500

    added riot api key

--- api-files/axios.js
+++ api-files/axios.js
@@ -1,9 +1,9 @@
-const axios = require('axios');
-require('dotenv').config({path: __dirname + '/../.env'});
+const axios = require("axios");
+require("dotenv").config({ path: __dirname + "/../.env" });
 
-const instance = axios.create( {
-    baseURL: 'https://na1.api.riotgames.com/lol',
-    params: {api_key: RGAPI-ff8251e1-c23c-46d3-a3c7-6d66de7bd7fa},
+const instance = axios.create({
+  baseURL: "https://na1.api.riotgames.com/lol",
+  params: { api_key: process.env.API_KEY },
 });
 
 module.exports = instance;
