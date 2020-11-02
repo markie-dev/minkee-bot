5980e93c33fd52052373e005aa296629ac2c3ba8
marcus <marcus.d.ellison@gmail.com>
2020-11-01 23:46:33 -0600

    updated some messages

--- components/tweetMessage.js
+++ components/tweetMessage.js
@@ -1,13 +1,19 @@
 let winMessages = [
   "MINKEE KANG TO CHALLENGER???",
   "MINKEE finally stopped Feeding!",
+  "winners queue",
   "GO MINKEE GO MINKEE GO ",
+  "minkee diff",
 ];
 
 let loseMessages = [
+  "elo terrorist minkee kang strikes again!",
   "You gonna post the highlights on your reddit LOL? https://www.reddit.com/user/bahksu",
+  "minkee diff",
+  "minkee kang boosted to D4?!?!",
   "Please don't queue again bro lmfao",
   "Time to head to bed minkee...",
+  "Spamming ranked will NOT make the pain go away",
   "Here’s a highlight clip of your game! https://www.youtube.com/watch?v=mcHIDzGwFlQ",
 ];
 
