e23b449c46818f50a6d86a499f04c7bd923380c1
marcus <marcus.d.ellison@gmail.com>
2020-11-01 22:44:27 -0600

    maybe working condition

--- components/tweetMessage.js
+++ components/tweetMessage.js
@@ -1,23 +1,17 @@
 let winMessages = [
-    "Okay, congratulations but when are you gonna be challenger?",
-    "WOW! @BTS_twt COULD NEVER",
-    "WOW! @lol_tyler1 TAKE SOME NOTES",
-    "WOW! @Maxwaldo_ CONSIDER HIM A NEW CLIENT?! @Trieuloo",
-    "WOW! @LSXYZ9 CONSIDER HIM A NEW CLIENT?! @Trieuloo",
-    "POG POG POG POG still trash POG POG POG POG",
+  "MINKEE KANG TO CHALLENGER???",
+  "MINKEE finally stopped Feeding!",
+  "GO MINKEE GO MINKEE GO ",
 ];
 
 let loseMessages = [
-    "Poopy Woopy, you just lost some LP",
-    "https://gph.is/2AUXdSp",
-    "Your gameplay needs some work! Try following this tutorial: https://www.youtube.com/watch?v=FWJufSUDW28",
-    "Your gameplay needs some work! Try following this tutorial: https://www.youtube.com/watch?v=gpXKwOQbqSY",
-    "You: http://www.youtube.com/watch?v=QpumKceAyog&t=0m23s",
-    "Sadness: http://www.youtube.com/watch?v=v3UXBJtZE8I&t=0m28s",
-    "Here’s a highlight clip of your game! https://www.youtube.com/watch?v=mcHIDzGwFlQ",
+  "You gonna post the highlights on your reddit LOL? https://www.reddit.com/user/bahksu",
+  "Please don't queue again bro lmfao",
+  "Time to head to bed minkee...",
+  "Here’s a highlight clip of your game! https://www.youtube.com/watch?v=mcHIDzGwFlQ",
 ];
 
 module.exports = {
-    winMessages: winMessages,
-    loseMessages: loseMessages
+  winMessages: winMessages,
+  loseMessages: loseMessages,
 };
--- messages.js
+++ messages.js
@@ -1,44 +1,45 @@
 let { getAccount } = require("./api-files/riot.js");
-let { winMessages, loseMessages} = require("./components/tweetMessage.js");
-let summonerName = 'Trieuloo'
+let { winMessages, loseMessages } = require("./components/tweetMessage.js");
+let summonerName = "Tameedrocus";
 
 module.exports.getMessage = getMessage;
 
 async function getMessage() {
-    let gameData = await getAccount(summonerName);
-    let message;
+  let gameData = await getAccount(summonerName);
+  let message;
 
-    //depending on win or lose, append string message
-    if (gameData.win) {
-        message = winMessages[Math.floor(Math.random() * winMessages.length)];
-        message += "\n\nCongrats on Winning! Maybe you might hit challenger this season =) \n\n";
-    } else {
-        message = loseMessages[Math.floor(Math.floor(Math.random() * loseMessages.length))];
-        message += "\n\nSorry you lost. Make sure you don't tilt to D1 again =) \n\n";
+  //depending on win or lose, append string message
+  if (gameData.win) {
+    message = winMessages[Math.floor(Math.random() * winMessages.length)];
+    message += "\n\nRare Minkee W \n\n";
+  } else {
+    message =
+      loseMessages[Math.floor(Math.floor(Math.random() * loseMessages.length))];
+    message += "\n\nLOL you lost again?? \n\n";
+  }
+
+  message += "@minkeekang stats: \n";
+  let gameStat = Object.entries(gameData);
+  let gameId = gameData.gameId;
+  for (i in gameStat) {
+    if (i != 3) {
+      let camel = gameStat[i][0].replace(/([A-Z])+/g, " $1");
+      let normal = camel.charAt(0).toUpperCase() + camel.slice(1);
+      gameStat[i][0] = normal;
     }
+  }
+  for (i = 4; i < 7; i++) {
+    message += gameStat[i][0] + ": " + gameStat[i][1] + "\n";
+  }
 
-    message += "@Trieuloo stats: \n";
-    let gameStat = Object.entries(gameData)
-    let gameId = gameData.gameId;
-    for (i in gameStat){
-        if(i != 3){
-            let camel = gameStat[i][0].replace(/([A-Z])+/g, " $1");
-            let normal = camel.charAt(0).toUpperCase() + camel.slice(1);
-            gameStat[i][0] = normal
-        }
-    }
-    for(i = 4; i < 7; i++) {
-        message += gameStat[i][0] + ": " + gameStat[i][1] + "\n";
-    }
+  message +=
+    "\n" + gameStat[2][1] + " / " + gameStat[3][0] + ": " + gameStat[3][1];
 
-    message += "\n" + gameStat[2][1] + " / " + gameStat[3][0] + ": " + gameStat[3][1];
+  let messageData = {
+    gameId: gameId,
+    message: message,
+  };
 
-    let messageData = {
-        gameId: gameId,
-        message: message
-    };
-
-    // console.log(message);
-    return messageData;
+  // console.log(message);
+  return messageData;
 }
-
