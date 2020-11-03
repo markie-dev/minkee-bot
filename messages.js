145b2de543dd80050e2c894b161b81d7a6de2975
marcus <marcus.d.ellison@gmail.com>
2020-11-02 21:24:22 -0600

    added cs per min stat back

--- messages.js
+++ messages.js
@@ -28,10 +28,7 @@ async function getMessage() {
       gameStat[i][0] = normal;
     }
   }
-  for (i = 4; i < 7; i++) {
-    message += gameStat[i][0] + ": " + gameStat[i][1] + "\n";
-  }
-  for (i = 7; i < 8; i++) {
+  for (i = 4; i < 8; i++) {
     message += gameStat[i][0] + ": " + gameStat[i][1] + "\n";
   }
 
